5da07831d2a8c980e72ee42e30d342ca808619a3
gopikarp <gokurprabhu@gmail.com>
2024-11-29 23:59:22 +0530

    3rd commit

--- week1,2/9_for_exercise.py
+++ week1,2/9_for_exercise.py
@@ -15,3 +15,23 @@ for x in range(1,10):
       continue
     print(x*x)
 
+#3 Your monthly expense list (from Jan to May) looks like this,
+expense_list = [2340, 2500, 2100, 3100, 2980]
+months = ["Jan", "Feb", "Mar", "Apr", "May"]
+# Write a program that asks you to enter an expense amount and program should tell
+# you in which month that expense occurred. If expense is not found then it should print that as well.
+month_list = ["January", "February", "March", "April", "May"]
+expense_list = [2340, 2500, 2100, 3100, 2980]
+e = input("Enter expense amount: ")
+e = int(e)
+
+month = -1
+for i in range(len(expense_list)):
+    if e == expense_list[i]:
+        month = i
+        break
+
+if month != -1:
+    print(f'You spent {e} in {month_list[month]}')
+else:
+    print(f'You didn\'t spend {e} in any month')
